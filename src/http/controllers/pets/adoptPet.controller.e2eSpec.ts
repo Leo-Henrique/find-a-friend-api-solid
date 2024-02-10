7b125424cf2dc085554b2638c0a6d7179338d160
Leonardo Henrique <leonardo0507.business@gmail.com>
2024-02-10 13:46:47 -0300

    test: create e2e tests for pet

---
+++ src/http/controllers/pets/adoptPet.controller.e2eSpec.ts
@@ -0,0 +1,23 @@
+import { app } from "@/app";
+import { createAndAuthenticateOrg } from "@/http/test/createAndAuthenticateOrg";
+import { prisma } from "@/lib/prisma";
+import request from "supertest";
+import { describe, expect, it } from "vitest";
+import { petSpec } from "@/utils/test/entities"; 
+
+describe("Adopt pet (E2E)", () => {
+  it("should be able to adopt pet", async () => {
+    const { org, authorization } = await createAndAuthenticateOrg(app);
+
+    const pet = await prisma.pet.create({
+      data: { orgId: org.id, ...petSpec },
+    });
+
+    const response = await request(app.server)
+      .patch(`/pets/${pet.id}/adoptions`)
+      .set("Authorization", authorization)
+      .send();
+
+    expect(response.statusCode).toEqual(204);
+  });
+});
