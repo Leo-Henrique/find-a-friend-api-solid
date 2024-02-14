098ca1607dee7608bc5d2269878429f7abcdf8e1
Leonardo Henrique <leonardo0507.business@gmail.com>
2024-02-14 11:33:21 -0300

    fix: remove not used factory

--- src/useCases/factories/updatePetUseCase.factory.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaPetsRepository } from "@/repositories/prisma/prismaPets.repository";
-import { UpdatePetUseCase } from "../updatePet.useCase";
-
-export function updatePetUseCaseFactory() {
-  const petsRepository = new PrismaPetsRepository();
-
-  return new UpdatePetUseCase(petsRepository)
-}
